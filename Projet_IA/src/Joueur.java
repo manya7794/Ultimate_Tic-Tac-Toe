2db1a3a5dc618df7b9c688804b6df10e4fd75fe5
Trystan D <72400479+manya7794@users.noreply.github.com>
2021-02-28 01:13:48 +0100

    Ajout de la méthode de tir

--- Projet_IA/src/Joueur.java
+++ Projet_IA/src/Joueur.java
@@ -12,5 +12,33 @@ public class Joueur {
 		this.plateauAdverse= new Plateau();
 	}
 	
+		public Joueur(Plateau adv) {
+		this.plateauJoueur= new Plateau();
+		this.plateauAdverse= adv;
+	}
 	//Methodes
+	
+	/*
+	 * Effectue un tir sur une case definie par le joueur
+	 * @param i l'abscisse de la case
+	 * @param j l'ordonnee de la case 
+	 */
+	public void Tir(int i, int j) {
+		switch (Case.getCaseType(plateauAdverse, i, j)) {
+		//Case Mer
+		case 0:
+			//Remplace par une case vide
+			Case.setCaseVide(plateauAdverse, i, j);
+			break;
+		//Case Bateau
+		case 1:
+			//Remplace par une case detruite
+			Case.setCaseDetruit(plateauAdverse, i, j);
+			break;
+		//Tire deja effectue sur la case
+		default:
+			System.out.println("Un tir a deja ete effectue sur la case. Choisissez une autre case.");
+			break;
+		}
+	}
 }
