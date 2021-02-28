429f732d9ce804e01ee84ac398f24677eff305b9
Trystan D <72400479+manya7794@users.noreply.github.com>
2021-02-28 01:15:23 +0100

    Ajout de TestJoueur

---
+++ Projet_IA/src/TestJoueur.java
@@ -0,0 +1,16 @@
+
+public class TestJoueur {
+
+	public static void main(String[] args) {
+		Plateau PlateauTest= new Plateau();
+		Case c1= new Case();
+		System.out.print(c1.getCaseType());
+		System.out.println();
+		System.out.println();
+		PlateauTest.getPlateau()[0][0].setCaseBateau();
+		Joueur j = new Joueur(PlateauTest);
+		j.Tir(0, 0);
+		System.out.println(j.plateauAdverse.toString());
+	}
+
+}
