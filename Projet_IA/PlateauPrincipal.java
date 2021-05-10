7ed7916931ce0783c9dcb1f4dea17574768e7d63
ClarkeZ <56111201+ClarkeZ@users.noreply.github.com>
2021-05-11 01:29:52 +0200

    Update PlateauPrincipal.java
    Ajout des méthodes initialisation() et verifPlateau()

--- Projet_IA/PlateauPrincipal.java
+++ Projet_IA/PlateauPrincipal.java
@@ -10,6 +10,27 @@ public class PlateauPrincipal extends Plateau<PlateauSub>{
 	}
 	
 	//Methodes
+	/**
+	 * Methode ajoutant les 9 plateaux dans le plateau principal
+	 */
+	public void initialisation() {
+		PlateauSub platSub = new PlateauSub();
+		for(int i = 0 ; i < 9 ; i++) {
+			plateau.add(platSub);
+		}
+	}
 	
+	/**
+	 * Methode verifiant si un plateau n'est plus utilisable (gagne ou rempli)
+	 */
+	public void verifPlateau() {
+		//Parcours tous les plateaux
+		for(int i = 0 ; i < 9 ; i++) {
+			//Condition verifiant si le plateau est gagnante ou remplie
+			if(plateau.get(i).verifZone() || plateau.get(i).verifZoneRemplie()) {
+				//TODO : Regarder comment "neutraliser" la zone
+			}
+		}
+	}
 
 }
