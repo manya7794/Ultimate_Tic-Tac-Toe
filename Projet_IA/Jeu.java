e8ff65cc25324ea06f0139399e3bfc259c820375
ClarkeZ <56111201+ClarkeZ@users.noreply.github.com>
2021-05-19 01:27:09 +0200

    Update Jeu.java
    - Split de la méthode JVJ
    - Ajout d'un constructeur prenant en compte le niveau de l'IA

--- Projet_IA/Jeu.java
+++ Projet_IA/Jeu.java
@@ -1,5 +1,6 @@
 package Projet_IA;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Jeu {
@@ -15,22 +16,36 @@ public class Jeu {
 	
 	private Scanner sc;
 	
+	//Valeur qui sera donner a chaque case / plateau
+	private Valeur val;
+	
 	//Constructeur
+	
+	//Constructeur utilise pour le JVJ
 	public Jeu() {
 		//Grand Plateau contenant les 9 petits plateaux
 		plateauP = new PlateauPrincipal();
-		//Petit plateau contenant les 9 cases
-		//plateauS = new PlateauSub();
+
 		//Joueur j1 commence la partie, avec le symbole "X"
 		j1 = new Joueur(true, -1);
 		//Joueur j2, joue en deuxieme, avec le sylbole "O"
-		j2 = new Joueur(false,1);
+		//j2 = new Joueur(false,1);
 		
-		//x = new Case();
-		//x.addSymbole(-1);
+		sc = new Scanner(System.in);
+	}
+	
+	//Constructeur utilise pour le jouer contre l'IA
+	public Jeu(int niveau) {
+		//Grand Plateau contenant les 9 petits plateaux
+		plateauP = new PlateauPrincipal();
+
+		//Joueur j1 commence la partie, avec le symbole "X"
+		j1 = new Joueur(true, -1);
+		//Joueur j2, joue en deuxieme, avec le sylbole "O"
+		//j2 = new Joueur(false,1);
 		
-	//	o = new Case();
-	//	o.addSymbole(1);
+		//Initialise la valeur correspondant au niveau de jeu
+		val = new Valeur(niveau);
 		
 		sc = new Scanner(System.in);
 	}
@@ -41,40 +56,37 @@ public class Jeu {
 	 * 
 	 */
 	public void joueurVersusJoueur() {
+		System.out.println("Tour de X\n");
 		affichageChoixZone();
-		System.out.println("\nTour de X\n");
-		int choixZone = sc.nextInt()-1;
+		int choixZone = choix();
 		int choixCase = choixZone;
 		boolean choixC = false;
 		boolean choixZ = false;
+		
 		//Tant que le plateau principal est jouable, la partie n'est pas terminee
 		while(!plateauP.verifZone() || !plateauP.verifZoneRemplie()) {
 
 			//Verifie si la prochaine Zone, est une zone libre, donc jouable
-			if( !plateauP.getPlateau().get(choixCase).verifZone() || plateauP.getPlateau().get(choixCase).verifZoneRemplie()) {
+			if( choixZ = zoneLibre(choixCase))
 				choixZone = choixCase;
-				choixZ = true;
-			}
-			else {
-				choixZ = false;
-			}
 			
+			//Si le choix de la zone n'est pas bonne, alors demande a l'utilisateur de choisir la zone
 			while(!choixZ) {
 				affichageChoixZone();
-				choixZone = sc.nextInt()-1;
+				//Demande a l'utilisateur de choisir la zone dans lequel il veut jouer
+				choixZone = choix();
 				//Verifie si la Zone choisie est libre, donc jouable
-				if( !plateauP.getPlateau().get(choixZone).verifZone() || plateauP.getPlateau().get(choixZone).verifZoneRemplie()) {
-					choixZ = true;
-				}
-				else {
-					choixZ = false;
-				}
+				choixZ = zoneLibre(choixZone);
 			}
 			choixZ = false;
 			
+			//Si le choix de la case n'est pas bonne, alors demande a l'utilisateur de choisir la case
 			while(!choixC) {
 				affichageZone(choixZone);
-				choixCase = sc.nextInt()-1;
+				
+				// Demande a l'utilisateur de choisir la case dans lequel il veut jouer
+				choixCase = choix();
+				
 				//Verifie que la case est vide
 				if(plateauP.getPlateau().get(choixZone).getCase(choixCase).verifContenu()) {
 					choixC = true;
@@ -88,28 +100,57 @@ public class Jeu {
 			//Tour du joueur 1
 			if(j1.tour) {
 				//Affiche la zone en cours et le choix de la case du joueur en cours
-				System.out.println("choixZ : "+choixZone+" choixC : "+ choixCase);
+				//System.out.println("choixZ : "+choixZone+" choixC : "+ choixCase);
 				//plateauP.getPlateau().get(choixZone).setCase(choixCase, j1.symbole);
 				plateauP.getPlateau().get(choixZone).getCase(choixCase).setCross();
 				//Desactive le tour du joueur 1
 				j1.tour = false;
-				System.out.println("\nTour de O\n");
+				System.out.println("Tour de O\n");
 			}
 			
 			//Tour du joueur 2
 			else {
 				//Affiche la zone en cours et le choix de la case du joueur en cours
-				System.out.println("choixZ : "+choixZone+" choixC : "+ choixCase);
+				//System.out.println("choixZ : "+choixZone+" choixC : "+ choixCase);
 				//plateauP.getPlateau().get(choixZone).setCase(choixCase, j2.symbole);
 				plateauP.getPlateau().get(choixZone).getCase(choixCase).setCircle();
 				//Active le tour du joueur 1
 				j1.tour = true;
-				System.out.println("\nTour de X\n");
+				System.out.println("Tour de X\n");
 			}
-			
-			
-			
+				
 		}
+	}
+	
+	/**
+	 * Methode demandant a l'utilisateur de choisir la prochaine zone/case dans lequel il souhaite jouer
+	 * 
+	 * @return le numero de la zone/case que l'utilisateur a choisit
+	 */
+	private int choix() {
+		int choix = -2;
+		boolean loop = true;
+		while(loop) {
+			choix = sc.nextInt()-1;
+			if(choix > -1 && choix < 9)
+				return choix;
+			else
+				System.err.println("\nValeur invalide, veuillez choisir entre 1 et 9\n");
+		}
+		return choix;
+	}
+	
+	/**
+	 * Methode verifiant si une zone est libre
+	 * 
+	 * @param choixZone la zone devant etre verifiee
+	 * @return true si la zone est libre, false sinon
+	 */
+	private boolean zoneLibre(int choixZone) {
+		if( !plateauP.getPlateau().get(choixZone).verifZone() || plateauP.getPlateau().get(choixZone).verifZoneRemplie()) 
+			return true;
+		else
+			return false;
 	}
 	
 	/**
@@ -119,7 +160,7 @@ public class Jeu {
 		//Affichage du plateau de jeu complet
 		System.out.println(plateauP.toString());
 		//Instructions
-		System.out.println("\nVeuillez choisir la zone sur lequel vous voulez jouer (1-9) : \n");
+		System.out.println("Veuillez choisir la zone sur lequel vous voulez jouer (1-9) : \n");
 	}
 	
 	/**
@@ -134,8 +175,7 @@ public class Jeu {
 		//Numero de la zone affichee
 		System.out.println("\nZone "+affiche+"\n");
 		System.out.println(plateauP.getPlateau().get(choix).affiche());
-		System.out.println("\nVeuillez choisir la case sur lequel vous voulez jouer (1-9) : \n");
+		System.out.println("Veuillez choisir la case sur lequel vous voulez jouer (1-9) : \n");
 	}
-	
 	
 }
