1f364fc20f7c903c715edbce6aea9dfd3f8721c8
Trystan D <72400479+manya7794@users.noreply.github.com>
2021-05-19 13:45:55 +0200

    Finalisation du max

--- Projet_IA/Minimax.java
+++ Projet_IA/Minimax.java
@@ -44,7 +44,6 @@ public class Minimax {
 		return caseFinale;
 	}
 	
-	//Reprendre en partie le first min
 	/**
 	 * Methode renvoyant le plus grand poids trouve pour la zone entree en argument
 	 * @param platP Plateau de jeu a etudier
@@ -69,14 +68,65 @@ public class Minimax {
 				
 				//Verification de la profondeur
 				if(profondeur>0) {
-					//Recuperer le poids de la zone
-					poidsFinal = max(platP, i, profondeur-1, symboleJoueur);
+					//Recuperer le poids min de la zone
+					int poidsTmp = min(platP, i, profondeur-1, symboleJoueur);
+					//Comparaison de ce poids au poids final actuel
+					if(poidsTmp>poidsFinal) {
+						poidsFinal=poidsTmp;
+					}
 				}
+				//Cas ou il s'agit de la derniere profondeur a analyser
+				else {
+					//Verifier valeur
+					int poidsZone=platSubClone.getValeur().getPoids();
+					if(poidsZone>poidsFinal) {
+						poidsFinal=poidsZone;
+					}
+				}
+			}
+		}
+		//Retourne la case ayant la plus faible valeur
+		return poidsFinal;
+	}
+	
+	/**
+	 * Methode renvoyant le plus petit poids trouve pour la zone entree en argument
+	 * @param platP Plateau de jeu a etudier
+	 * @param zone Numero de la zone a etudier
+	 * @param profondeur Nombre de recherches restantes
+	 * @param symboleJoueur Symbole du joueur a cocher dans la case
+	 * @return Le poids min trouve
+	 */
+	public int min(PlateauPrincipal platP, int zone, int profondeur, int symboleJoueur) {
+		//Poids de depart a comparer
+		int poidsFinal=500;
+		//On parcourt chaque case de la zone
+		for(int i=0; i<9;i++) {
+			//Creation d'un plateau clone pour effectuer la recherche
+			PlateauPrincipal platClone = new PlateauPrincipal(platP);
+			//Recuperation de la zone
+			PlateauSub platSubClone=platClone.getPlateau().get(zone); 
+			//Verifier que la case n'est pas deja remplie
+			if(platSubClone.getPlateau().get(i).verifContenu()) {
+				//Simuler cochage
+				platSubClone.getPlateau().get(i).setSymbole(symboleJoueur);
 				
-				//Verifier valeur
-				int poidsZone=platSubClone.getValeur().getPoids();
-				if(poidsZone>poidsFinal) {
-					poidsFinal=poidsZone;
+				//Verification de la profondeur
+				if(profondeur>0) {
+					//Recuperer le poids max de la zone
+					int poidsTmp = max(platP, i, profondeur-1, symboleJoueur);
+					//Comparaison de ce poids au poids final actuel
+					if(poidsTmp<poidsFinal) {
+						poidsFinal=poidsTmp;
+					}
+				}
+				//Cas ou il s'agit de la derniere profondeur a analyser
+				else {
+					//Verifier valeur
+					int poidsZone=platSubClone.getValeur().getPoids();
+					if(poidsZone<poidsFinal) {
+						poidsFinal=poidsZone;
+					}
 				}
 			}
 		}
