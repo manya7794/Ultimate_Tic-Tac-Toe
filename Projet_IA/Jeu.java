14206ff39be9682e2d885918fee095225b153e68
Trystan D <72400479+manya7794@users.noreply.github.com>
2021-05-20 22:38:06 +0200

    Division de JoueurvSIA - ajout de iaFacile()

--- Projet_IA/Jeu.java
+++ Projet_IA/Jeu.java
@@ -142,7 +142,7 @@ public class Jeu {
 	/**
 	 * Methode lancant la partie contre l'IA
 	 */
-	/*public void joueurVersusIA() {
+/*	public void joueurVersusIA() {
 		System.out.println("Tour de X\n");
 		affichageChoixZone();
 		//Choix de la zone de depart
@@ -226,48 +226,6 @@ public class Jeu {
 					
 					//IA de niveau 2
 					if(niveau==2) {
-						//Verification que pour chaque zone 2 cases sont moins cochees	
-						if(!activeAlphaBeta) {
-							//Verification de chaque zone
-							for(PlateauSub platS : plateauP.getPlateau()) {
-								//Nombre de cases cochees par zone
-								int nbCasesCochees=0;
-								//Verification de la case
-								for(Case c : platS.getPlateau()) {
-									if(c.getContenu()!=0) {
-										nbCasesCochees+=1;
-									}
-									if(nbCasesCochees>=2) {
-										activeAlphaBeta=true;
-									}
-								}
-							}
-						}
-						
-						//Recherche MiniMax si l'elagage n'est pas active
-						if(!activeAlphaBeta) {
-							// L'IA choisit une case en utilisant l'algorithme minimax
-							choixCase = minimax.firstMin(plateauP, choixZone, minimax.getProfondeur(), ia.symbole);
-							//Verifie que la case est vide
-							if(plateauP.getPlateau().get(choixZone).getCase(choixCase).verifContenu()) {
-								choixC = true;
-							}
-							//Case deja remplie
-							else
-								choixC = false;
-						}
-						//Elagage active
-						else {
-							// L'IA choisit une case en utilisant l'elagage
-							choixCase = alphabeta.firstBeta(plateauP, choixZone, minimax.getProfondeur(), ia.symbole);
-							//Verifie que la case est vide
-							if(plateauP.getPlateau().get(choixZone).getCase(choixCase).verifContenu()) {
-								choixC = true;
-							}
-							//Case deja remplie
-							else
-								choixC = false;
-						}
 
 					}
 					
@@ -344,10 +302,15 @@ public class Jeu {
 				
 		}
 	}
-*/	
+	*/
+	
 	public void joueurVersusIA() {
+		
 		switch(niveau) {
 		case 1:
+			iaFacile();
+			break;
+		case 2:
 			System.out.println("Tour de X\n");
 			affichageChoixZone();
 			//Choix de la zone de depart
@@ -357,104 +320,52 @@ public class Jeu {
 			//Booleen servent a la boucle pour choisir une case / une zone
 			boolean choixC = false;
 			boolean choixZ = false;
+			//Booleen activant l'elagage alphaBeta si l'ordinateur l'utilise
+			boolean activeAlphaBeta=false;
 			
-			//Tant que le plateau principal est jouable, la partie n'est pas terminee
-			while(!plateauP.verifZone() || !plateauP.verifZoneRemplie()) {
-
-				//Verifie si la prochaine Zone, est une zone libre, donc jouable, si pas jouable choixZ = false
-				if( choixZ = zoneLibre(choixCase))
-					choixZone = choixCase;
-				
-				//Si le choix de la zone n'est pas bonne, alors demande a l'utilisateur de choisir la zone
-				while(!choixZ) {
-					//Le joueur choisit la zone si c'est son tour
-					if(j1.getTour()) {
-						affichageChoixZone();
-						//Demande a l'utilisateur de choisir la zone dans lequel il veut jouer
-						choixZone = choix();
-						//Verifie si la Zone choisie est libre, donc jouable
-						choixZ = zoneLibre(choixZone);
-					}
-					//Sinon l'ia choisit la zone
-					else {
-						//choixZoneTmp sert pour comparer a choixZone
-						int choixZoneTmp = 500;
-						//Parcours les 9 zones
-						for(int i = 0 ; i < 9 ; i++) {
-							//Choix de la zone selon l'algorithme minimax
-							choixZone = minimax.firstMin(plateauP, i, minimax.getProfondeur(), ia.symbole);
-							//Compare choixZone a choixZoneTmp si plus petit alors on conserve, si identique alors on garde dans l'ordre d'arrive
-							if(minimax.getValeurZone() < choixZoneTmp) {
-								choixZoneTmp = minimax.getValeurZone();
-								choixZone = i;
-							}
+			//Verification que pour chaque zone 2 cases sont moins cochees	
+			if(!activeAlphaBeta) {
+				//Verification de chaque zone
+				for(PlateauSub platS : plateauP.getPlateau()) {
+					//Nombre de cases cochees par zone
+					int nbCasesCochees=0;
+					//Verification de la case
+					for(Case c : platS.getPlateau()) {
+						if(c.getContenu()!=0) {
+							nbCasesCochees+=1;
 						}
-						//Verifie si la Zone choisie par l'IA est libre, donc jouable
-						choixZ = zoneLibre(choixZone);
-					}
-				}
-				choixZ = false;
-
-				//Si le choix de la case n'est pas bonne, alors demande a l'utilisateur de choisir la case
-				while(!choixC) {
-					//Le joueur choisit la case
-					if(j1.getTour()) {
-						affichageZone(choixZone);
-						
-						//Demande a l'utilisateur de choisir la case dans lequel il veut jouer
-						choixCase = choix();
-						
-						//Verifie que la case est vide
-						if(plateauP.getPlateau().get(choixZone).getCase(choixCase).verifContenu()) {
-							choixC = true;
+						if(nbCasesCochees>=2) {
+							activeAlphaBeta=true;
 						}
-						//Case deja remplie
-						else
-							choixC = false;
-					}
-					//L'IA choisit la case
-					else {
-						//L'IA choisit une case en utilisant l'algorithme minimax
-						choixCase = minimax.firstMin(plateauP, choixZone, minimax.getProfondeur(), ia.symbole);
-						System.out.println(choixCase);
-						//Verifie que la case est vide
-						if(plateauP.getPlateau().get(choixZone).getCase(choixCase).verifContenu()) {
-							choixC = true;
-						}
-						//Case deja remplie
-						else
-							choixC = false;
 					}
 				}
-				choixC = false;
-				
-				//Tour du joueur 1
-				if(j1.getTour()) {
-					//Affecte la croix a la zone et a la case choisit par le joueur 1
-					plateauP.getPlateau().get(choixZone).getCase(choixCase).setCross();
-					//Desactive le tour du joueur
-					j1.setTour(false);
-					//System.out.println("Tour de l'IA");
-					//System.out.println(plateauP.toString());
-				}
-				
-				//Tour de l'IA
-				else {
-					//Choisit la case en fonction de l'algorithme minimax 
-					choixCase = minimax.firstMin(plateauP, choixZone, minimax.getProfondeur(), ia.symbole);
-					//Affectation de la case par l'IA
-					plateauP.getPlateau().get(choixZone).getCase(choixCase).setCircle();
-					
-					System.out.println("L'IA a jouer sur la case : "+(choixCase+1)+" de la zone : "+(choixZone+1));
-					//Active le tour du joueur 
-					j1.setTour(true);
-					//System.out.println("Tour de X\n");
-					System.out.println("C'est a votre tour de jouer\n");
-				}
-					
 			}
-			break;
-		case 2:
+			
+			//Recherche MiniMax si l'elagage n'est pas active
+			if(!activeAlphaBeta) {
+				// L'IA choisit une case en utilisant l'algorithme minimax
+				choixCase = minimax.firstMin(plateauP, choixZone, minimax.getProfondeur(), ia.symbole);
+				//Verifie que la case est vide
+				if(plateauP.getPlateau().get(choixZone).getCase(choixCase).verifContenu()) {
+					choixC = true;
+				}
+				//Case deja remplie
+				else
+					choixC = false;
+			}
+			//Elagage active
+			else {
+				// L'IA choisit une case en utilisant l'elagage
+				choixCase = alphabeta.firstBeta(plateauP, choixZone, minimax.getProfondeur(), ia.symbole);
+				//Verifie que la case est vide
+				if(plateauP.getPlateau().get(choixZone).getCase(choixCase).verifContenu()) {
+					choixC = true;
+				}
+				//Case deja remplie
+				else
+					choixC = false;
+			}
+
 			break;
 		case 3:
 			break;
@@ -570,5 +481,112 @@ public class Jeu {
 		//Affiche la zone
 		System.out.println(plateauP.getPlateau().get(choix).affiche());
 		System.out.println("Veuillez choisir la case sur lequel vous voulez jouer (1-9) : \n");
+	}
+	
+	public void iaFacile() {
+		System.out.println("Tour de X\n");
+		affichageChoixZone();
+		//Choix de la zone de depart
+		int choixZone = choix();
+		//Initialisation de la case par rapport a la zone de depart
+		int choixCase = choixZone;
+		//Booleen servent a la boucle pour choisir une case / une zone
+		boolean choixC = false;
+		boolean choixZ = false;
+		
+		//Tant que le plateau principal est jouable, la partie n'est pas terminee
+		while(!plateauP.verifZone() || !plateauP.verifZoneRemplie()) {
+
+			//Verifie si la prochaine Zone, est une zone libre, donc jouable, si pas jouable choixZ = false
+			if( choixZ = zoneLibre(choixCase))
+				choixZone = choixCase;
+			
+			//Si le choix de la zone n'est pas bonne, alors demande a l'utilisateur de choisir la zone
+			while(!choixZ) {
+				//Le joueur choisit la zone si c'est son tour
+				if(j1.getTour()) {
+					affichageChoixZone();
+					//Demande a l'utilisateur de choisir la zone dans lequel il veut jouer
+					choixZone = choix();
+					//Verifie si la Zone choisie est libre, donc jouable
+					choixZ = zoneLibre(choixZone);
+				}
+				//Sinon l'ia choisit la zone
+				else {
+					//choixZoneTmp sert pour comparer a choixZone
+					int choixZoneTmp = 500;
+					//Parcours les 9 zones
+					for(int i = 0 ; i < 9 ; i++) {
+						//Choix de la zone selon l'algorithme minimax
+						choixZone = minimax.firstMin(plateauP, i, minimax.getProfondeur(), ia.symbole);
+						//Compare choixZone a choixZoneTmp si plus petit alors on conserve, si identique alors on garde dans l'ordre d'arrive
+						if(minimax.getValeurZone() < choixZoneTmp) {
+							choixZoneTmp = minimax.getValeurZone();
+							choixZone = i;
+						}
+					}
+					//Verifie si la Zone choisie par l'IA est libre, donc jouable
+					choixZ = zoneLibre(choixZone);
+				}
+			}
+			choixZ = false;
+
+			//Si le choix de la case n'est pas bonne, alors demande a l'utilisateur de choisir la case
+			while(!choixC) {
+				//Le joueur choisit la case
+				if(j1.getTour()) {
+					affichageZone(choixZone);
+					
+					//Demande a l'utilisateur de choisir la case dans lequel il veut jouer
+					choixCase = choix();
+					
+					//Verifie que la case est vide
+					if(plateauP.getPlateau().get(choixZone).getCase(choixCase).verifContenu()) {
+						choixC = true;
+					}
+					//Case deja remplie
+					else
+						choixC = false;
+				}
+				//L'IA choisit la case
+				else {
+					//L'IA choisit une case en utilisant l'algorithme minimax
+					choixCase = minimax.firstMin(plateauP, choixZone, minimax.getProfondeur(), ia.symbole);
+					//Verifie que la case est vide
+					if(plateauP.getPlateau().get(choixZone).getCase(choixCase).verifContenu()) {
+						choixC = true;
+					}
+					//Case deja remplie
+					else
+						choixC = false;
+				}
+			}
+			choixC = false;
+			
+			//Tour du joueur 1
+			if(j1.getTour()) {
+				//Affecte la croix a la zone et a la case choisit par le joueur 1
+				plateauP.getPlateau().get(choixZone).getCase(choixCase).setCross();
+				//Desactive le tour du joueur
+				j1.setTour(false);
+				//System.out.println("Tour de l'IA");
+				//System.out.println(plateauP.toString());
+			}
+			
+			//Tour de l'IA
+			else {
+				//Choisit la case en fonction de l'algorithme minimax 
+				choixCase = minimax.firstMin(plateauP, choixZone, minimax.getProfondeur(), ia.symbole);
+				//Affectation de la case par l'IA
+				plateauP.getPlateau().get(choixZone).getCase(choixCase).setCircle();
+				
+				System.out.println("L'IA a joue sur la case : "+(choixCase+1)+" de la zone : "+(choixZone+1));
+				//Active le tour du joueur 
+				j1.setTour(true);
+				//System.out.println("Tour de X\n");
+				System.out.println("C'est a votre tour de jouer\n");
+			}
+				
+		}
 	}
 }
