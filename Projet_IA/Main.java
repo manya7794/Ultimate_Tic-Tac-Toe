bdc33741d4f378ec0662540eae76197aa04509ed
Trystan D <72400479+manya7794@users.noreply.github.com>
2021-05-20 19:16:32 +0200

    Finalisation des menus

--- Projet_IA/Main.java
+++ Projet_IA/Main.java
@@ -19,8 +19,31 @@ public class Main {
 		int choix=choixPrincipal(sc);
 		//Cas ou l'utilisateur veut jouer contre l'ordinateur
 		if(choix==1) {
-
+			//Choix du niveau de difficulte
+			choix=choixDifficulte(sc);
+			//Mode facile
+			if(choix==1) {
+				//Creation d'une nouvelle partie avec une IA de niveau 1
+				Jeu j1=new Jeu(1);
+				//Lancement de la partie
+				j1.joueurVersusJoueur();
+			}
+			//Mode intermediaire
+			else if(choix==2) {
+				//Creation d'une nouvelle partie avec une IA de niveau 1
+				Jeu j1=new Jeu(2);
+				//Lancement de la partie
+				j1.joueurVersusJoueur();
+			}
+			//Mode difficile
+			else {
+				//Creation d'une nouvelle partie avec une IA de niveau 1
+				Jeu j1=new Jeu(3);
+				//Lancement de la partie
+				j1.joueurVersusJoueur();
+			}
 		}
+		//Cas ou l'utilisateur veut jouer contre un autre humain	
 		else if(choix==2) {
 			//Creation d'une nouvelle partie
 			Jeu j2=new Jeu();
@@ -30,7 +53,7 @@ public class Main {
 	}
 	
 	/**
-	 * Methode affichant recuperant le choix de l'utilisateur 
+	 * Methode recuperant le choix de l'utilisateur 
 	 * au niveau du menu principal
 	 * @param sc Scanner recuperant le choix
 	 * @return Choix de l'utilisateur
@@ -64,5 +87,43 @@ public class Main {
 	public static void afficheMenuPrincipal() {
 		System.out.println("1.Mode 1 joueur");
 		System.out.println("2.Mode 2 joueurs");
+	}
+
+	/**
+	 * Methode recuperant le choix de l'utilisateur
+	 * au niveau du menu de choix du niveau de l'IA
+	 * @param sc Scanner recuperant le choix
+	 * @return
+	 */
+	public static int choixDifficulte(Scanner sc) {
+		boolean sortie=false;
+		String choix =null;
+		System.out.println("Selectionnez le niveau de difficulte de l'IA que vous voulez affronter :");
+		while(!sortie) {
+			afficheMenuDifficulte();
+			try {
+				choix=sc.nextLine();
+			}catch (InputMismatchException exception) {
+				System.out.println(choix+" n'est pas une entree valide");
+			}
+			
+			if((choix.equals("1"))||(choix.equals("2"))||(choix.equals("3"))) {
+				sortie=true;
+			}
+			else {
+				System.out.println(choix+" n'est pas une entree valide.\n");
+				System.out.println("Selectionnez le niveau de difficulte de l'IA que vous voulez affronter :");
+			}
+		}
+		return Integer.valueOf(choix);
+	}
+	
+	/**
+	 * Methode affichant le menu de choix de la difficulte
+	 */
+	public static void afficheMenuDifficulte() {
+		System.out.println("1.Mode facile");
+		System.out.println("2.Mode intermediaire");
+		System.out.println("3.Mode difficile");
 	}
 }
